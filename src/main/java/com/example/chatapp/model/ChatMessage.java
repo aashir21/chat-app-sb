5ca6ecbc4d8414baee0063cfb9a7b67eee0b072d
Aashir <aashir_sidik@hotmail.com>
2023-09-19 16:55:54 +0100

    Added initial files

---
+++ src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.example.chatapp.model;
+
+import lombok.*;
+import org.apache.logging.log4j.message.Message;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+}
---
+++ src/main/java/com/example/chatapp/model/MessageType.java
@@ -0,0 +1,10 @@
+package com.example.chatapp.model;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+
+}
